2bb55bbd816e5a784124f419b1670f642d2d7d15
luchanso <oleg@loutchansky.com>
2016-04-22 17:13:55 +0300

    Remove trash from express file

--- src/express.js
+++ src/express.js
@@ -4,12 +4,6 @@ let express = require('express'),
   winston = require('./winston'),
   app = express();
 
-const port = process.env.PORT || 80;
-
 app.use(express.static('./public'));
 
-app.listen(port, () => {
-  winston.info(`App run on 0.0.0.0:${port}`);
-});
-
-module.exports = express;
+module.exports = app;
